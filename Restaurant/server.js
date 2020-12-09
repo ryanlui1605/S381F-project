8ec149b9afc797a9f059f1f18689bcc35c26f06d
ramwong <s1260229@ouhk.edu.hk>
2020-12-09 20:43:05 +0800

    add cookie-session to prevent user who have not logged in go outside loginpage and register page

--- Restaurant/server.js
+++ Restaurant/server.js
@@ -5,16 +5,64 @@ const session = require('cookie-session');
 
 const mongodbConnect = require('./utils/db').mongodbConnect;
 const userRoutes = require('./routes/user');
+const key1 = "ui3bqoithj2q4piofmnlskdanmgvm#Q$ty'p1o03jgp'dfbvmdZ:Fmb";
+const key2 = "ifhaosihguiqwgrh98vhnoi3n4tgh309123-.1-3123-o1-.412-312";
+
+// handler for RESTful services
+// not sure is it correct
+
+app.get(/api\/restaurant\/name\/.*/, (req, res) => {
+    const parseUrl = url.parse(req.url, true);
+
+    //get restaurant name
+    const name = parseUrl.pathname.replace('/api/restaurant/name/', '');
+
+    res.send(name);
+});
+
+app.get(/api\/restaurant\/borough\/.*/, (req, res) => {
+    const parseUrl = url.parse(req.url, true);
+
+    //get restaurant borough
+    const borough = parseUrl.pathname.replace('/api/restaurant/borough/', '');
+
+    res.send(borough);
+})
+
+app.get(/api\/restaurant\/cuisine\/.*/, (req, res) => {
+    const parseUrl = url.parse(req.url, true);
+
+    //get restaurant cuisine
+    const cuisine = parseUrl.pathname.replace('/api/restaurant/cuisine/', '');
+
+    res.send(cuisine);
+})
+
+// end of handler for RESTful services
 
 // set default engine to view ejs
 app.set('view engine', 'ejs');
-// app.use(session({
-//     name: 'user',
-// }))
-// handler for having page
+app.use(session({
+    name: 'user',
+    keys: [key1, key2],
+    maxAge: 24 * 60 * 60 * 1000 //24h * 60m * 60s * 1000ms = 24hours
+}))
 
+// routes for login and register
 app.use(userRoutes);
 
+//if not requiring homepage or register page, check cookie
+app.use(/\/.+/, (req,res,next)=>{
+    console.log(req.session.username);
+    if(!req.session.username){  
+        //username is empty
+        res.redirect('/'); 
+        next();
+    }else{
+        //username is not empty
+        next();
+    }
+})
 // page to show 
 // user name
 // number of document
@@ -135,39 +183,9 @@ app.get('/remove', (req, res) => {
     res.send('Delete was successful');
 });
 
-// end of handler for having page
+// end of handler for having webpage
 
-// handler for RESTful services
-// not sure is it correct
 
-app.get(/api\/restaurant\/name\/.*/, (req, res) => {
-    const parseUrl = url.parse(req.url, true);
-
-    //get restaurant name
-    const name = parseUrl.pathname.replace('/api/restaurant/name/', '');
-
-    res.send(name);
-});
-
-app.get(/api\/restaurant\/borough\/.*/, (req, res) => {
-    const parseUrl = url.parse(req.url, true);
-
-    //get restaurant borough
-    const borough = parseUrl.pathname.replace('/api/restaurant/borough/', '');
-
-    res.send(borough);
-})
-
-app.get(/api\/restaurant\/cuisine\/.*/, (req, res) => {
-    const parseUrl = url.parse(req.url, true);
-
-    //get restaurant cuisine
-    const cuisine = parseUrl.pathname.replace('/api/restaurant/cuisine/', '');
-
-    res.send(cuisine);
-})
-
-// end of handler for RESTful services
 
 
 
